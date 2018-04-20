d92f752479274fc7872516dc28a9cdba2c6168a5
Tara Ellicott <taraellicott@gmail.com>
2018-04-20 04:45:39 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,7 @@ def oxford_comma(array)
   # 2  element - "and"
   # 3 or more - oxford commas
   if array.length == 1 
-    "#{array[0}"
+    "#{array[0]}"
   elsif array.length == 2 
     "#{array[0]} and #{array[1]}"
 #   else array.length > 2 
