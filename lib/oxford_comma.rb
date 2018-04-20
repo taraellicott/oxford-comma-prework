8defd5fd98ca91f090dbe663f9e7ce24919a8266
Tara Ellicott <taraellicott@gmail.com>
2018-04-20 04:40:52 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,6 +4,13 @@ def oxford_comma(array)
   # 1 element - no formatting
   # 2  element - "and"
   # 3 or more - oxford commas
+  if array.length == 1 
+    "#{array[0}""
+  elsif array.length == 2 
+    "#{array[0} and #{array[1}"
+  else array.length > 2 
+    
+
 string = array.split(",")
 string.each do |x|
   x.
