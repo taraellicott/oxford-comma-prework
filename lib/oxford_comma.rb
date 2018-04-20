dea7bcffdbd14966c41c7c18f7504966735139d7
Tara Ellicott <taraellicott@gmail.com>
2018-04-20 04:59:35 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,24 +1,24 @@
 require "pry"
 
-# def oxford_comma(array)
-#   # 1 element - no formatting
-#   # 2  element - "and"
-#   # 3 or more - oxford commas
-#   if array.length == 1 
-#     "#{array[0]}"
-#   elsif array.length == 2 
-#     return "#{array[0]} and #{array[1]}"
-#   else array.length > 2 
-#     array[-1].insert(0, "and")
-#   end
-#   array.join(",")
-# end
-
 def oxford_comma(array)
-  if array.length == 2
+  # 1 element - no formatting
+  # 2  element - "and"
+  # 3 or more - oxford commas
+  if array.length == 1 
+    "#{array[0]}"
+  elsif array.length == 2 
     return "#{array[0]} and #{array[1]}"
-  elsif 2 < array.length
+  else array.length > 2 
     array[-1].insert(0, "and ")
   end
-  array.join(", ")    
+  array.join(", ")
 end
+
+# def oxford_comma(array)
+#   if array.length == 2
+#     return "#{array[0]} and #{array[1]}"
+#   elsif 2 < array.length
+#     array[-1].insert(0, "and ")
+#   end
+#   array.join(", ")    
+# end
