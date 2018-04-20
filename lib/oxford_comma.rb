a5dd45bd0ac32fe274b1fca7b03bf7a116a240d3
Tara Ellicott <taraellicott@gmail.com>
2018-04-20 04:36:47 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,11 @@
 require "pry"
 
 def oxford_comma(array)
+  # 1 element - no formatting
+  # 2  element - "and"
+  # 3 or more - oxford commas
 string = array.split(",")
 string.each do |x|
-  
+  x.
 end
 end
