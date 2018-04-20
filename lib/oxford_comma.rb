dab4f27a709819dd8320bf165d876b070a077a0a
Tara Ellicott <taraellicott@gmail.com>
2018-04-20 04:53:06 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -9,8 +9,7 @@ def oxford_comma(array)
   elsif array.length == 2 
     "#{array[0]} and #{array[1]}"
   else array.length > 2 
-    #{array[-1]}.
-
-# string = array.split(",")
-end
+    #{array[-1]}.insert(0, "and")
+  end
+  array.join(",")
 end
