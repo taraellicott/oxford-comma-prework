e8e9c07a8d82e483189a8ccb1f25b6dcd69fa4d0
Tara Ellicott <taraellicott@gmail.com>
2018-04-20 04:58:36 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -13,3 +13,12 @@ require "pry"
 #   end
 #   array.join(",")
 # end
+
+def oxford_comma(array)
+  if array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif 2 < array.length
+    array[-1].insert(0, "and ")
+  end
+  array.join(", ")    
+end
